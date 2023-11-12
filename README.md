3b6cf96bb963896834db6c7cdc213956a3a0eb0a
vpurplee <vitriaferreira1402@gmail.com>
2023-11-12 20:15:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Projeto criado com a trilha do REACT da DIO
+
+Criei essa calculadora com o auxilio da trilha de React da dio
+
+
+
---
+++ src/global.js
@@ -0,0 +1,8 @@
+import {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+    *, body{
+        marghin: 0;
+        padding: 0;
+    }
+`;
